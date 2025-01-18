aa5f6a5500f2e887bf8163e4a425a8a1c369a3b3
sapphiriza <sapphiriza@gmail.com>
2025-01-18 15:49:50 -0800

    Version 1.2.1: Update remnants for Space Garden and fix K2A version.

--- prototypes/entity/remnants.lua
+++ prototypes/entity/remnants.lua
@@ -2,29 +2,23 @@ data:extend({
   {
     type = "corpse",
     name = "space-garden-remnants",
-    icon = "__Krastorio2Assets__/buildings/greenhouse/greenhouse.png",
+    icon = "__Krastorio2Assets__/icons/entities/greenhouse.png",
     icon_size = 64, icon_mipmaps = 4,
     flags = {"placeable-neutral", "not-on-map"},
+    hidden_in_factoriopedia = true,
     subgroup = "production-machine-remnants",
     order = "a-g-a",
-    selection_box = {{-4, -4}, {4, 4}},
+    selection_box = {{-3.5, -3.5}, {3.5, 3.5}},
     tile_width = 3,
     tile_height = 3,
     selectable_in_game = false,
     time_before_removed = 60 * 60 * 15, -- 15 minutes
     final_render_layer = "remnants",
     remove_on_tile_placement = false,
-    animation = make_rotated_animation_variations_from_sheet (2, {
-      filename = "__Krastorio2Assets__/remnants/big-random-pipes-remnant/big-random-pipes-remnant.png",
-      line_length = 1,
-      width = 266,
-      height = 196,
+    animation = util.sprite_load("__space-age__/graphics/entity/biochamber/biochamber-remnants",{
+      scale = 0.875,
       frame_count = 1,
-      variation_count = 1,
-      axially_symmetrical = false,
       direction_count = 1,
-      shift = util.by_pixel(7, 5.5),
-      scale = 0.5
-    })
+    }),
   }
 })
