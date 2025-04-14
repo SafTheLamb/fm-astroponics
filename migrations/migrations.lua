06f159e71db4a346459488e83a6bb9374ab9695d
sapphiriza <sapphiriza@gmail.com>
2025-04-14 11:40:32 -0700

    Version 1.6.1: Cleanup

--- migrations/migrations.lua
+++ migrations/migrations.lua
@@ -2,5 +2,7 @@ for _,force in pairs(game.forces) do
   local technologies = force.technologies
   local recipes = force.recipes
 
-  recipes["ice-melting"].enabled = technologies["astroponics"].researched
+  if technologies["astroponics"].researched then
+    recipes["ice-melting"].enabled = true
+  end
 end
